b6991997df4cb42df08c889e13a7122e295e0e88
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-17 16:31:15 +0000

    Read xml data file

--- SmartHome/SmartHome/ViewModel/MainWindowViewModel.cs
+++ SmartHome/SmartHome/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace SmartHome.ViewModel
+﻿using System.Xml;
+
+namespace SmartHome.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        public MainWindowViewModel()
+        {
+            ReadXML();
+        }
+
+        void ReadXML()
+        {
+            XmlReader xmlReader = XmlReader.Create("...\\...\\Data\\Data.xml");
+            while (xmlReader.Read())
+            {
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Floor"))
+                {
+                    if (xmlReader.HasAttributes)
+                    {
+                        string name = xmlReader.GetAttribute("Name");
+                    }
+                }
+            }
+        }
+
     }
 }
