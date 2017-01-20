862f4b8ef0d10309e769f9793b494e61ad2688c8
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-20 13:10:41 +0000

    Add icon library and control strip

---
+++ SmartHome/SmartHome/UserControl/ControlStripUserControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SmartHome.UserControl
+{
+    /// <summary>
+    /// Interaction logic for ControlStripUserControl.xaml
+    /// </summary>
+    public partial class ControlStripUserControl 
+    {
+        public ControlStripUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
