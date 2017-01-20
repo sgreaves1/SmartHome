ed22211d452140c85736997dc83f1c3a3b4413ab
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-20 13:30:46 +0000

    Add the new window to add an element

--- SmartHome/SmartHome/UserControl/ControlStripUserControl.xaml.cs
+++ SmartHome/SmartHome/UserControl/ControlStripUserControl.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using SmartHome.Command;
+using SmartHome.View;
+
 namespace SmartHome.UserControl
 {
     /// <summary>
@@ -13,6 +17,12 @@ namespace SmartHome.UserControl
         public ControlStripUserControl()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddElementWindow window = new AddElementWindow();
+            window.ShowDialog();
         }
     }
 }
---
+++ SmartHome/SmartHome/View/AddElementWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace SmartHome.View
+{
+    /// <summary>
+    /// Interaction logic for AddElementWindow.xaml
+    /// </summary>
+    public partial class AddElementWindow
+    {
+        public AddElementWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
