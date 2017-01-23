9e105801dc3096becc0c9a9a9ef187b388c0700f
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-23 12:22:33 +0000

    Add device base class

---
+++ SmartHome/SmartHome/Model/DeviceBase.cs
@@ -0,0 +1,62 @@
+﻿namespace SmartHome.Model
+{
+    public class DeviceBase : BaseModel, IDevice
+    {
+        private bool _isOnline;
+        private string _name;
+        private int _x;
+        private int _y;
+
+        public bool IsOnline
+        {
+            get
+            {
+                return _isOnline;
+            }
+            set
+            {
+                _isOnline = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ SmartHome/SmartHome/Model/IDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model
+{
+    public interface IDevice
+    {
+        bool IsOnline { get; set; }
+        string Name { get; set; }
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
