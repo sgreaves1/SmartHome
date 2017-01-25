cb9e8fc7ed9762029436fad08248a02b17d1db3d
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-25 12:10:22 +0000

    Update fake pi code to run asynchronously

--- SmartHome/FakePi/FakePiCode.cs
+++ SmartHome/FakePi/FakePiCode.cs
@@ -1,30 +1,60 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FakePi
 {
     public class FakePiCode
     {
-        private TcpClient clientSocket = new TcpClient();
-
         public FakePiCode()
         {
+            Task task = new Task(Heartbeat);
+            task.Start();
+        }
 
-            Console.WriteLine("Client Started");
-            clientSocket.Connect("127.0.0.1", 8989);
-            Console.WriteLine("Client Socket Program - Server Connected ...");
+        static async void Heartbeat()
+        {
+            Task task = HeartbeatAsync();
+            await task;
+        }
 
-            NetworkStream serverStream = clientSocket.GetStream();
-            byte[] outStream = Encoding.ASCII.GetBytes("192.168.0.15: Connected" + "$");
-            serverStream.Write(outStream, 0, outStream.Length);
-            serverStream.Flush();
+        static async Task HeartbeatAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient clientSocket = new TcpClient();
+                    Console.WriteLine("Client Started");
+                    clientSocket.Connect("127.0.0.1", 8989);
+                    Console.WriteLine("Client Socket Program - Server Connected ...");
 
-            byte[] inStream = new byte[10025];
-            serverStream.Read(inStream, 0, (int) clientSocket.ReceiveBufferSize);
-            string returndata = Encoding.ASCII.GetString(inStream);
-            Console.WriteLine(returndata);
-            Console.WriteLine("");
+                    while (true)
+                    {
+                        NetworkStream serverStream = clientSocket.GetStream();
+                        byte[] outStream = Encoding.ASCII.GetBytes("192.168.0.15: HeartBeat" + "$");
+                        Console.WriteLine("Heartbeat");
+                        serverStream.Write(outStream, 0, outStream.Length);
+                        serverStream.Flush();
+
+                        Thread.Sleep(3000);
+                    }
+
+                    //byte[] inStream = new byte[10025];
+                    //serverStream.Read(inStream, 0, (int) clientSocket.ReceiveBufferSize);
+                    //string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                    //Console.WriteLine(returndata);
+                    //Console.WriteLine("");
+                }
+                catch (Exception)
+                {
+
+                }
+
+                Thread.Sleep(5000);
+            }
         }
     }
 }
