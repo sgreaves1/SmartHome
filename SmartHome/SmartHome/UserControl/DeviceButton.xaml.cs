bbc7d63a46cf8fc91b1a350d8bdc831b4fa8e41e
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-01-26 11:10:01 +0000

    Add device button user control to replace hard coded buttons

---
+++ SmartHome/SmartHome/UserControl/DeviceButton.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using SmartHome.Model;
+
+namespace SmartHome.UserControl
+{
+    /// <summary>
+    /// Interaction logic for DeviceButton.xaml
+    /// </summary>
+    public partial class DeviceButton
+    {
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DeviceModelProperty =
+            DependencyProperty.Register("DeviceModel", 
+                typeof(IDevice), 
+                typeof(DeviceButton), 
+                new PropertyMetadata(null));
+        
+        public DeviceButton()
+        {
+            InitializeComponent();
+        }
+
+        public IDevice DeviceModel
+        {
+            get { return (IDevice)GetValue(DeviceModelProperty); }
+            set { SetValue(DeviceModelProperty, value); }
+        }
+    }
+}
