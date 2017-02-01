b19e302fe1128aa60c3ebe6d4d1e82e1dbbe6ec9
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-02-01 14:23:18 +0000

    added more buttons to radio ui

--- SmartHome/SmartHome/ViewModel/RadioUIViewModel.cs
+++ SmartHome/SmartHome/ViewModel/RadioUIViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using SmartHome.Command;
 using SmartHome.Model;
 using TcpServer;
@@ -28,16 +29,44 @@ namespace SmartHome.ViewModel
             }
         }
 
+        public ICommand DownCommand { get; set; }
+        public ICommand BackCommand { get; set; }
+        public ICommand PlayCommand { get; set; }
         public ICommand SkipCommand { get; set; }
+        public ICommand UpCommand { get; set; }
 
         private void InitCommands()
         {
+            DownCommand = new RelayCommand(DownCommandExecute);
+            BackCommand = new RelayCommand(BackCommandExecute);
+            PlayCommand = new RelayCommand(PlayCommandExecute);
             SkipCommand = new RelayCommand(SkipCommandExecute);
+            UpCommand = new RelayCommand(UpCommandExecute);
+        }
+
+        private void DownCommandExecute()
+        {
+            _server.SendMessage(Radio.Ip, "Down");
+        }
+
+        private void BackCommandExecute()
+        {
+            _server.SendMessage(Radio.Ip, "Back");
+        }
+
+        private void PlayCommandExecute()
+        {
+            _server.SendMessage(Radio.Ip, "Play");
         }
 
         private void SkipCommandExecute()
         {
             _server.SendMessage(Radio.Ip, "Skip");
+        }
+
+        private void UpCommandExecute()
+        {
+            _server.SendMessage(Radio.Ip, "Up");
         }
     }
 }
